d703f5c2e576b709e9121dcc60edbace7641b826
Balakumaran <bkumaran121@gmail.com>
2024-03-10 14:21:55 +0530

    feat: Add Config file

---
+++ config/data.js
@@ -0,0 +1,60 @@
+export const newsData = [
+  {
+    id: 1,
+    title: 'All you Need To Know About Night Photography on Smartphones',
+    image: require('./../assets/images/iphone.jpg'),
+    date: '2 Days ago',
+    author: 'James Bowen',
+    avator: require('./../assets/images/bmw.jpg'),
+    content:
+      'This specification does not indicate the behavior, rendering or otherwise, of space characters other than those explicitly identified here as white space characters. For this reason, authors should use appropriate elements and styles to achieve visual formatting effects that involve white space, rather than space characters',
+  },
+  {
+    id: 2,
+    title: 'All you Need To Know About Night Photography on Smartphones',
+    image: require('./../assets/images/ocean.jpg'),
+    date: '2 Days ago',
+    author: 'James Bowen',
+    avator: require('./../assets/images/mountain.jpg'),
+    content:
+      'This specification does not indicate the behavior, rendering or otherwise, of space characters other than those explicitly identified here as white space characters. For this reason, authors should use appropriate elements and styles to achieve visual formatting effects that involve white space, rather than space characters',
+  },
+  {
+    id: 3,
+    title: 'All you Need To Know About Night Photography on Smartphones',
+    image: require('./../assets/images/drone.jpg'),
+    date: '2 Days ago',
+    author: 'James Bowen',
+    avator: require('./../assets/images/bmw.jpg'),
+    content:
+      'This specification does not indicate the behavior, rendering or otherwise, of space characters other than those explicitly identified here as white space characters. For this reason, authors should use appropriate elements and styles to achieve visual formatting effects that involve white space, rather than space characters',
+  },
+]
+
+export const exploreData = [
+  {
+    id: 1,
+    title: 'Tech',
+    image: require('./../assets/images/drone.jpg'),
+  },
+  {
+    id: 2,
+    title: 'Sports',
+    image: require('./../assets/images/football.jpg'),
+  },
+  {
+    id: 3,
+    title: 'Food',
+    image: require('./../assets/images/cake.jpg'),
+  },
+  {
+    id: 4,
+    title: 'Science',
+    image: require('./../assets/images/iphone.jpg'),
+  },
+  {
+    id: 5,
+    title: 'Music',
+    image: require('./../assets/images/piano.jpg'),
+  },
+]
